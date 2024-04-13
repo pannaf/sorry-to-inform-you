69b1a5d0584d1eb60009b84e4a96756a25869419
Tino Purmann <tinopurmann@gmail.com>
2024-04-13 16:42:47 -0700

    added phone validation

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,26 @@ def configure_page():
     interview_guide = ""
 
     job_description = st.text_area("Job description")
+    st.divider()
+    st.markdown("### Candidate info 💁")
+    columns =  st.columns(2)
+    candidate_name = columns[0].text_input("Candidate name")
+    candidate_phone = columns[1].text_input("Phone number", placeholder=5105707879)
+    if candidate_phone:
+        if len(candidate_phone) != 10:
+            st.error("Please enter a 10-digit phone number")
+        else:
+            try:
+                candidate_phone = int(candidate_phone)
+            except ValueError:
+                st.error("Please enter a valid phone number")
+
+
     if st.button("Submit"):
         st.spinner("Submitting job description...")
         handle_configure_click()
+
+
 
     if not interview_guide == "":
         st.write(interview_guide)
