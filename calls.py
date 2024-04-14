98536abfde15e96ab86c595868e7bbe0300dbeb3
Tino Purmann <tinopurmann@gmail.com>
2024-04-13 18:49:49 -0700

    makes calls and gets transcripts

--- calls.py
+++ calls.py
@@ -13,6 +13,10 @@ def place_call(to_number):
         to_number=to_number,
         override_agent_id="c2a3bb9ef69532220dde5eeb26a9c289",
     )
-    return call.agent_id
+    return call.call_id
 
-place_call(number_test)
+
+def get_transcript(call_id):
+    client = Retell(api_key=os.getenv("RETELL_API_KEY"))
+    call = client.call.retrieve("119c3f8e47135a29e65947eeb34cf12d", )
+    return call.transcript
