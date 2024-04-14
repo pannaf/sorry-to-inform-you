98b6bf615931f006a6e7bea10fd89e4c13fa5e02
Christopher Bassano <cb@ChristophersMBP.localdomain>
2024-04-13 18:19:45 -0700

    Format results_page

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def configure_page():
     if "candidate_name" not in st.session_state:
         st.session_state['candidate_name'] = None
 
-    st.markdown("## Submit Job Possdfdsting ⚡")
+    st.markdown("## Submit Job Posting ⚡")
     interview_guide = ""
 
     job_description = st.text_area("Job description")
@@ -42,18 +42,32 @@ def configure_page():
     if st.button("Submit", disabled=st.session_state.button_state_disabled):
         st.spinner("Submitting job description...")
         interview_questions = handle_configure_click()
-
+        st.session_state.page = "results_page"
+        
 
     if not interview_guide == "":
         st.write(interview_guide)
 
-
+feedback = {'feedback': 'The candidate struggled to provide concrete examples of successful upselling and handling difficult customer situations. They also admitted to having difficulty staying organized and engaging with customers proactively. The candidate demonstrated a lack of initiative in collaborative tasks and could benefit from improving their patience and understanding with customers.', 'recommendation': False, 'reason': "The candidate's lack of experience and skills in upselling, customer interaction, and teamwork are concerning for a retail position. They displayed limited enthusiasm for the job and a relatively low job fit score, indicating a potential mismatch for the role.", 'top relevant skills': ['Politeness', 'Availability'], 'potential skill gaps': ['Upselling', 'Customer Service', 'Organization', 'Teamwork', 'Patience'], 'enthusiasm': 2, 'job fit': 2, 'communication': 3}
 def results_page():
+    st.divider()
     st.markdown("## Interview Results ⚡")
+    st.write("### Feedback")
+    st.write(feedback['feedback'])
+    st.write("### Recommendation")
+    st.write(feedback['recommendation'])
+    st.write("### Reason")
+    st.write(feedback['reason'])
+    st.write("### Top Relevant Skills")
+    st.write(feedback['top relevant skills'])
+    st.write("### Potential Skill Gaps")
+    st.write(feedback['potential skill gaps'])
 
 def main():
     if st.session_state.page == "configure_page":
         configure_page()
+    if st.session_state.page == "results_page":
+        results_page()
 
 
 if __name__ == "__main__":
