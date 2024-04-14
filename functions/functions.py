dc654808fcf71db0317c65243544e50d6581d5d7
pannaf <panna@cr8tr.ai>
2024-04-13 18:16:13 -0700

    hooked up streamlit w/question generator

--- functions/functions.py
+++ functions/functions.py
@@ -1,3 +1,15 @@
 import streamlit as st
-def handle_configure_click():
-    st.write("You clicked the Submit button")
+from tune2 import InterviewAssistant
+
+
+def handle_configure_click(job_description, behavioral_requirements):
+    assistant = InterviewAssistant()
+
+    with st.spinner("Getting questions for interviewer...."):
+        questions = assistant.get_questions(job_description, behavioral_requirements)
+        st.write("Generated Questions:", questions)
+
+    # assessment = assistant.assess_call(call_transcript, job_description, behavior_characteristics)
+
+    # print("Generated Questions:", questions)
+    # print("Call Assessment:", assessment)
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,17 @@ from functions.functions import handle_configure_click
 if "page" not in st.session_state:
     st.session_state.page = "configure_page"
 
+
 def configure_page():
 
     if "button_state_disabled" not in st.session_state:
-        st.session_state['button_state_disabled'] = True
+        st.session_state["button_state_disabled"] = True
 
     if "phone" not in st.session_state:
-        st.session_state['phone'] = None
+        st.session_state["phone"] = None
 
     if "candidate_name" not in st.session_state:
-        st.session_state['candidate_name'] = None
+        st.session_state["candidate_name"] = None
 
     st.markdown("## Submit Job Posting ⚡")
     interview_guide = ""
@@ -22,7 +23,7 @@ def configure_page():
     behavioral_requirements = st.text_area("Behavioral Requirements")
     st.divider()
     st.markdown("### Candidate info 💁")
-    columns =  st.columns(2)
+    columns = st.columns(2)
     st.session_state.candidate_name = columns[0].text_input("Candidate name")
     candidate_phone = columns[1].text_input("Phone number", placeholder=5105707879)
     if candidate_phone:
@@ -34,30 +35,21 @@ def configure_page():
             except ValueError:
                 st.error("Please enter a valid phone number")
 
-
     if st.session_state.phone and st.session_state.candidate_name:
-        st.session_state['button_state_disabled'] = False
-
+        st.session_state["button_state_disabled"] = False
 
     if st.button("Submit", disabled=st.session_state.button_state_disabled):
         st.spinner("Submitting job description...")
-        handle_configure_click()
-
-
+        handle_configure_click(job_description, behavioral_requirements)
 
     if not interview_guide == "":
         st.write(interview_guide)
-
-
 
 
 def main():
 
     if st.session_state.page == "configure_page":
         configure_page()
-
-
-
 
 
 if __name__ == "__main__":
